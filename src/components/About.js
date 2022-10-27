72b5b7f983db2634a226ddce116cc0e580e465e8
dangkhoa023 <107237640+dangkhoa023@users.noreply.github.com>
2022-10-27 10:09:31 +0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -17,8 +17,9 @@ function About() {
       <div className="container-desc">
         <p className="context">Name: Nguyễn Trương Đăng Khoa</p>
         <p className="context">MSSV: 46.01.104.086</p>
-        <p className="context">Trường: ĐHSP TP.HCM</p>
-        <p className="context">KHOA:CNTT</p>
+        <p className="context">Trường: ĐHSP Tp.HCM </p>
+        <p className="context">Khoa :Công nghệ thông tin </p>
+        <p className="context">Lớp: 2211COMP1031 </p>
       </div>
     </div>
   </div>
